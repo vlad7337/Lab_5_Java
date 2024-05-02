3e2c8c8193c0b1840e353d2c07baa529de9cc8ce
vlad7337 <114061697+vlad7337@users.noreply.github.com>
2024-05-02 17:16:50 +0300

    Update Lab5Controller.java

--- src/main/java/ua/lab5/javafxlab5/Lab5Controller.java
+++ src/main/java/ua/lab5/javafxlab5/Lab5Controller.java
@@ -9,22 +9,12 @@ import ua.lab5.javafxlab5.functions.core.Function;
 
 public class Lab5Controller {
     @FXML
-    private Label welcomeText;
-    @FXML
-    private RadioButton radioButton1;
-    @FXML
-    private RadioButton radioButton2;
+    private Label calculateLabel;
+
     private Function func = MyFunctions.firstFunction();
 
     @FXML
-    public void init(){
-        ToggleGroup toggleGroup = new ToggleGroup();
-        radioButton1.setToggleGroup(toggleGroup);
-        radioButton2.setToggleGroup(toggleGroup);
-    }
-
-    @FXML
     protected void onSelectButtonClick() {
-        welcomeText.setText(String.format("%s", func.calculate(1.65)));
+        calculateLabel.setText(String.format("%s", func.calculate(1.65)));
     }
 }
